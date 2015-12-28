41da2b778c44f8f8a8029ac8cfd47dd57e89880f
Priya Jyoti <Priya.Jyoti@happiestminds.com>
2015-12-28 15:25:55 +0530

    commit

--- db_test/Tables/dbo.Table_1.sql
+++ db_test/Tables/dbo.Table_1.sql
@@ -4,3 +4,13 @@ CREATE TABLE [dbo].[Table_1]
 [wage1] [money] NULL
 ) ON [PRIMARY]
 GO
+
+INSERT INTO dbo.Table_1
+(emp1,wage1)
+VALUES
+('Tom', 100); 
+ 
+ INSERT INTO dbo.Table_1
+(emp1,wage1)
+VALUES
+('Jerry', 200);
